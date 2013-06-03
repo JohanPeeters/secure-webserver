a16a36488b030b6166322c3b1345b0326f3486ef
yo <yo@johanpeeters.com>
2013-06-03 17:27:27 +0200

    bootstrap script reports its activity

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#if [ ! -e /etc/apt/sources.list.d/puppet-main-precise.list ]; then 
-#	echo "deb http://apt.puppetlabs.com/ precise main" > /etc/apt/sources.list.d/puppet-main-precise.list; 
-#fi
-#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1054B7A24BD6EC30
+echo 'updating apt-get packages'
 apt-get update
+echo 'installing rubygems'
 apt-get -y install rubygems=1.8.15-1ubuntu0.1
-#apt-get -y install puppet=2.7.21-1puppetlabs1
+echo 'installing puppet'
 gem install --version '= 3.2.1' puppet --no-ri --no-rdoc
+echo 'installing puppet modules'
 puppet module install puppetlabs/apt --version 1.1.0
