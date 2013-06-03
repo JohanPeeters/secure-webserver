d542c77473efac24926096a198052d24c41ad1fb
yo <yo@johanpeeters.com>
2013-06-03 17:14:35 +0200

    vanilla installation of nginx

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,32 +1,38 @@
-class nginx ($version, $host = 'secappdev.org'){
-  # in order to install reasonably recent versions of nginx,
-  # impervious to recent security advisories,
-  # we need to add an apt repository.
-  
+class nginx ($version, $host){
+	# in order to install reasonably recent versions of nginx,
+	# impervious to recent security advisories,
+	# we need to add an apt repository.
 
-  package {'nginx':
-    ensure => $version
-  }
-  file {'/etc/nginx/sites-enabled/default':
-   ensure => absent,
-   require => Package['nginx']
-  }
-  file {'/etc/nginx/sites-available/default':
-    ensure => absent,
-    require => File['/etc/nginx/sites-enabled/default'],
-  }
-  file {"/etc/nginx/sites-available/$host":
-    ensure => present,
-    content => template('nginx/host.conf.erb'),
-    require => Package['nginx']
-  }
-  file {"/etc/nginx/sites-enabled/$host":
-    ensure => link,
-    target => "/etc/nginx/sites-available/$host"
-  }
-  service {'nginx':
-    ensure => running,
-    enable => true,
-    subscribe => File["/etc/nginx/sites-available/$host"],
-  }
+
+	package {'nginx':
+		ensure => $version
+	}
+	file {"/etc/nginx/sites-available":
+		ensure => directory,
+	}
+	file {"/etc/nginx/sites-enabled":
+		ensure => directory,
+	}
+	file {'/etc/nginx/sites-enabled/default':
+		ensure => absent,
+		require => Package['nginx']
+	}
+	file {'/etc/nginx/sites-available/default':
+		ensure => absent,
+		require => File['/etc/nginx/sites-enabled/default'],
+	}
+	file {"/etc/nginx/sites-available/$host":
+		ensure => present,
+		content => template('nginx/host.conf.erb'),
+		require => Package['nginx']
+	}
+	file {"/etc/nginx/sites-enabled/$host":
+		ensure => link,
+		target => "/etc/nginx/sites-available/$host"
+	}
+	service {'nginx':
+		ensure => running,
+		enable => true,
+		subscribe => File["/etc/nginx/sites-available/$host"],
+	}
 }
--- site.pp
+++ site.pp
@@ -1,6 +1,18 @@
 node 'ubuntu-12' {
-  class{'nginx':
-    version => '1.4.1',
-  }
+	class{'nginx':
+		version => '1.4.1-1~precise',
+		host    => 'localhost',
+		require => Apt::Source['nginx'],
+	}
+	apt::source { 'nginx':
+		location                => 'http://nginx.org/packages/ubuntu/',
+		release               => 'precise',
+		repos               => 'nginx',
+		#required_packages => 'debian-keyring debian-archive-keyring',
+		#key                    => '55BE302B',
+		#key_server           => 'subkeys.pgp.net',
+		#pin                => '-10',
+		include_src      => false
+	}
 }
 
