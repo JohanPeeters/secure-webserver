e7ef71a59a3322771dcc19d3b8430450a65d05a4
yo <yo@johanpeeters.com>
2013-06-03 16:45:40 +0200

    vagrant and bootstrap working, puppet nginx module fail

--- Vagrantfile
+++ Vagrantfile
@@ -47,7 +47,7 @@ Vagrant.configure("2") do |config|
   #   vb.customize ["modifyvm", :id, "--memory", "1024"]
   # end
   # 
-  #config.vm.provision :shell, :path => "bootstrap.sh"
+  config.vm.provision :shell, :path => "bootstrap.sh"
   #
   # View the documentation for the provider you're using for more
   # information on available options.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-apt-get install puppet=2.7.11
+#if [ ! -e /etc/apt/sources.list.d/puppet-main-precise.list ]; then 
+#	echo "deb http://apt.puppetlabs.com/ precise main" > /etc/apt/sources.list.d/puppet-main-precise.list; 
+#fi
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1054B7A24BD6EC30
+apt-get update
+apt-get -y install rubygems=1.8.15-1ubuntu0.1
+#apt-get -y install puppet=2.7.21-1puppetlabs1
+gem install --version '= 3.2.1' puppet --no-ri --no-rdoc
 puppet module install puppetlabs/apt --version 1.1.0
