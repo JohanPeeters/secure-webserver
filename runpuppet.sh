8594c836e3936564f5c772cc814bfc9305f6dec9
supernelis <nelis@archiwise.com>
2013-06-14 11:09:20 +0200

    fix voor relatieve paths

--- runpuppet.sh
+++ runpuppet.sh
@@ -1,2 +1,3 @@
-PUPPET_SCRIPTS=/vagrant/production
+#PUPPET_SCRIPTS=/vagrant/production
+PUPPET_SCRIPTS=$(dirname $0)/production
 sudo puppet apply $PUPPET_SCRIPTS/manifests/site.pp --modulepath $PUPPET_SCRIPTS/modules/:/home/vagrant/.puppet/modules:/usr/share/puppet/modules:/etc/puppet/modules/
