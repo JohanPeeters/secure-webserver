a5fc22c20e0c1bfdaa3f664c449409fc05f5c58a
supernelis <nelis@archiwise.com>
2013-06-14 11:47:32 +0200

    fix for relative paths + bash

--- runpuppet.sh
+++ runpuppet.sh
@@ -1,3 +1,5 @@
+#!/bin/bash  
+
 #PUPPET_SCRIPTS=/vagrant/production
 PUPPET_SCRIPTS=$(dirname $0)/production
 sudo puppet apply $PUPPET_SCRIPTS/manifests/site.pp --modulepath $PUPPET_SCRIPTS/modules/:/home/vagrant/.puppet/modules:/usr/share/puppet/modules:/etc/puppet/modules/
--- sanity-test.sh
+++ sanity-test.sh
@@ -1,5 +1,11 @@
-export VAGRANT_BASE=$(dirname $0)
-VAGRANT_BASE=$1
+#!/bin/bash  
+
+if [ -n "$1" ]; then
+	VAGRANT_BASE=$1
+else 
+	VAGRANT_BASE=$(dirname $0)
+fi	
+
 echo "** running sanity tests **"
 rspec $VAGRANT_BASE/spec/nginx.rb
 rspec $VAGRANT_BASE/spec/csr.rb
