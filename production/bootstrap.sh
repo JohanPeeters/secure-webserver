b8965e8307c7bd5feab9ac7c2cd18d93cb82ea1e
yo <yo@johanpeeters.com>
2013-07-28 16:17:06 +0200

    typo in bootstrap script stopping causing execution to abort

--- production/bootstrap.sh
+++ production/bootstrap.sh
@@ -9,7 +9,7 @@ echo 'Vulnerable to CommonName attack'
 echo 'So we uninstall'
 apt-get --purge remove ruby1.8
 apt-get autoremove
-echo "** all versions of Ruby available on Ubuntu or Debian via official apt-get repositories are known to be insecure'
+echo '** all versions of Ruby available on Ubuntu or Debian via official apt-get repositories are known to be insecure'
 echo 'so install from source.'
 echo 'Not giving up on managing packages entirely, so first install checkinstall.'
 apt-get -y install checkinstall=1.6.2-3ubuntu1
@@ -29,3 +29,5 @@ echo '** installing puppet'
 gem install --version '= 3.2.1' puppet --no-ri --no-rdoc
 echo '** installing puppet modules'
 puppet module install puppetlabs/apt --version 1.2.0
+echo '** installing rspec'
+gem install --version '=2.14.4' rspec --no-ri --no-rdoc
