2f3f972baf3aa54672d485c8fc587dad267c4737
yo <yo@johanpeeters.com>
2013-07-29 14:39:02 +0200

    detect root directory

--- make_installation_package.sh
+++ make_installation_package.sh
@@ -1,3 +1,4 @@
+ROOT_DIR=`dirname $0`
 apt-get -y install checkinstall=1.6.2-3ubuntu1
 cd /tmp
 wget -c --progress=bar http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p247.tar.gz
@@ -7,5 +8,5 @@ cd ruby-2.0.0-p247
 checkinstall -y \
 	--pkgversion 2.0.0-p247 \
 	--provides "ruby-interpreter" \
-	--pakdir=/vagrant
+	--pakdir=$ROOT_DIR
 make clean
--- production/bootstrap.sh
+++ production/bootstrap.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+ROOT_DIR=`dirname $0`/..
 echo '** updating apt-get packages'
 apt-get update
 echo '** all versions of Ruby available on Ubuntu or Debian via official apt-get repositories are known to be insecure'
 echo 'so install from source.'
 echo 'All ruby versions available on official repositories are Vulnerable to CommonName attack'
 echo '** installing ruby 2.0.0-p247'
-pkg_file=/vagrant/ruby-2.0.0_2.0.0-p247-1_amd64.deb
+pkg_file=${ROOT_DIR}/ruby-2.0.0_2.0.0-p247-1_amd64.deb
 if [ -e $pkg_file ]; then
 	dpkg -i $pkg_file;
 else
-	/vagrant/make_installation_package.sh;
+	${ROOT_DIR}/make_installation_package.sh;
 fi
 echo '** installing puppet'
 gem install --version '= 3.2.1' puppet --no-ri --no-rdoc
