029f3caa3a5263da1fe1ecae658e824bc9fd5f11
yo <yo@johanpeeters.com>
2013-07-29 09:47:17 +0200

    no ruby in base box

--- production/bootstrap.sh
+++ production/bootstrap.sh
@@ -2,15 +2,9 @@
 
 echo '** updating apt-get packages'
 apt-get update
-echo '** basebox (Ubuntu 12.04.2 - Precise Pangolin) runs an insecure version of Ruby, '
-echo 'see CVE-2013-4073'
-echo 'or http://www.ruby-lang.org/en/news/2013/06/27/hostname-check-bypassing-vulnerability-in-openssl-client-cve-2013-4073/'
-echo 'Vulnerable to CommonName attack'
-echo 'So we uninstall'
-apt-get --purge remove ruby1.8
-apt-get autoremove
 echo '** all versions of Ruby available on Ubuntu or Debian via official apt-get repositories are known to be insecure'
 echo 'so install from source.'
+echo 'All ruby versions available on official repositories are Vulnerable to CommonName attack'
 echo 'Not giving up on managing packages entirely, so first install checkinstall.'
 apt-get -y install checkinstall=1.6.2-3ubuntu1
 echo '** installing ruby 2.0.0-p247'
