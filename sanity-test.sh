7c3efbce1d22aebf6bd4916f2c36836582756942
yo <yo@johanpeeters.com>
2013-08-16 11:19:31 +0200

    root permissions are not necessary for tests

--- sanity-test.sh
+++ sanity-test.sh
@@ -8,11 +8,11 @@ fi
 pushd $VAGRANT_BASE
 echo "** running sanity tests **"
 
-sudo rspec --format documentation spec/csr.rb
-sudo rspec --format documentation spec/ca.rb
-sudo rspec --format documentation spec/crt.rb
-sudo rspec --format documentation spec/nginx.rb
-sudo rspec --format documentation spec/ciphersuites.rb
-sudo rspec --format documentation spec/nginx_config.rb
+rspec --format documentation spec/csr.rb
+rspec --format documentation spec/ca.rb
+rspec --format documentation spec/crt.rb
+rspec --format documentation spec/nginx.rb
+rspec --format documentation spec/ciphersuites.rb
+rspec --format documentation spec/nginx_config.rb
 
 popd
