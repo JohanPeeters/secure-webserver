692827df527f6a8674b27988c11ad84302bbd3bd
supernelis <nelis@archiwise.com>
2013-08-16 20:20:48 +0200

    Nicer formatting for less in demo

--- runpuppet.sh
+++ runpuppet.sh
@@ -2,4 +2,5 @@
 
 PUPPET_PROD=$(dirname $0)/production
 PUPPET_TEST=$(dirname $0)/test
-sudo puppet apply $PUPPET_TEST/manifests/site.pp --modulepath  $PUPPET_TEST/modules/:$PUPPET_PROD/modules/:/home/vagrant/.puppet/modules:/etc/puppet/modules/
+sudo puppet apply $PUPPET_TEST/manifests/site.pp \
+  --modulepath $PUPPET_TEST/modules/:$PUPPET_PROD/modules/:/home/vagrant/.puppet/modules:/etc/puppet/modules/
